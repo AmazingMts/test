d445f78894f1729a46d6645cdfd2fdc0b2afb3a6
Tianshi Miao <mts591227021@gmail.com>
2024-11-01 18:47:23 -0400

    Delete HW5 directory

--- HW5/src/cis5550/generic/Worker.java
+++
@@ -1,9 +0,0 @@
-package cis5550.generic;
-public class Worker {
-    public static void startPingThread(){
-        Thread pingThread = new Thread(()->{
-            while (true);
-        });
-        pingThread.start();
-    }
-}
